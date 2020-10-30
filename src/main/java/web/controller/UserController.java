92027f7bfb983ea5863ed008e4a3bd526d22d142
Solo-DmitriyK <maer-m@yandex.ru>
2020-10-30 14:22:10 +0300

    Initial commit

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,65 @@
+package web.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.servise.UserServise;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserServise userServise;
+
+    @Autowired
+    public UserController(UserServise userServise) {
+        this.userServise = userServise;
+    }
+
+    @GetMapping
+    public String allUser (Model model){
+        model.addAttribute("users", userServise.list());
+        return "users/users";
+    }
+
+    @GetMapping("/{id}")
+    public String show (@PathVariable("id") int id, Model model){
+        model.addAttribute("user", userServise.show(id));
+        return  "users/user";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user){
+        return "users/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("user") User user){
+        userServise.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id){
+        model.addAttribute("user", userServise.show(id));
+        return "users/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String udgate(@ModelAttribute("user") User user, @PathVariable("id") int id){
+        userServise.update(id, user);
+        return "redirect:/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id){
+        userServise.delete(id);
+        return "redirect:/users";
+    }
+
+
+
+}
---
+++ src/main/java/web/servise/UserServise.java
@@ -0,0 +1,45 @@
+package web.servise;
+
+import org.springframework.stereotype.Component;
+import web.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserServise {
+    private  static int COUNT;
+    private List<User> userList;
+
+    {
+        userList = new ArrayList<>();
+        userList.add(new User(++COUNT, "Den"));
+        userList.add(new User(++COUNT, "Ben"));
+        userList.add(new User(++COUNT, "Tom"));
+        userList.add(new User(++COUNT, "Tim"));
+
+    }
+
+    public List<User> list(){
+        return userList;
+    }
+
+    public User show(int id){
+        return userList.stream().filter(user -> user.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(User user){
+        user.setId(++COUNT);
+        userList.add(user);
+    }
+
+    public void update(int id, User updateUser){
+        User userToBeUpdated = show(id);
+        userToBeUpdated.setName(updateUser.getName());
+    }
+
+    public void delete(int id){
+        userList.removeIf(user -> user.getId()==id);
+    }
+
+}
